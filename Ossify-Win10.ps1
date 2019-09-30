222e1d52795b6be93617863c95e2e208acb5a20f
Lucas Hennessy <lhenness@purdue.edu>
2019-09-30 17:00:06 -0400

    Update Ossify-Win10.ps1
    Disables a bunch of services. May prevent night-light from working

--- Ossify-Win10.ps1
+++ Ossify-Win10.ps1
@@ -323,6 +323,106 @@ Write-Warning 'Setting UAC level to High...'
 	Write-Host 'Done.'
 
 #################################################################
+Write-Warning 'Disabling Windows services...'
+	[Array]$Services =
+		'lmhosts', # TCP/IP NetBIOS Helper
+		'wlidsvc', # Microsoft Account Sign-in Assistant
+		'SEMgrSvc', # Payments NFC/SE Manager
+		'tzautoupdate', # Auto Time Zone Updater
+		'AppVClient', # Microsoft App-V Client
+		'RemoteRegistry', # Remote Registry
+		'RemoteAccess', # Routing & Remote Access
+		'shpamsvc', # Shared PC Account Manager
+		'UevAgentService', # User Experience Virtualization Service
+		'WdiServiceHost', # Diagnostic Service Host
+		'WdiSystemHost', # Diagnostic System Host
+		'ALG', # Application Layer Gateway
+		'PeerDistSvc', # BranchCache
+		'Eaphost', # Extensible Authentication Protocol
+		'fdPHost', # Function Discovery Provider Host
+		'LxpSvc', # Language Experience Service
+		'lltdsvc', # Link-Layer Topology Discovery Mapper
+		'diagnosticshub.standardcollector.service', # Microsoft (R) Diagnostics Hub Standard Collector Service
+		'MSiSCSI', # Microsoft iSCSI Initiator Service
+		'WpcMonSvc', # WpcMonSvc
+		'PNRPsvc', # Peer Name Resolution Protocol
+		'p2psvc', # Peer Networking Grouping
+		'p2pimsvc', # Peer Networking Identity Manager
+		'PerfHost', # Performance Counter DLL Host
+		'pla', # Performance Logs & Alerts
+		'PNRPAutoReg', # PNRP Machine Name Publication
+		'PrintNotify', # PrintNotify
+		'wercplsupport', # Problem Reports & Solutions Control Panel
+		'TroubleshootingSvc', # Recommended Troubleshooting Service
+		'SessionEnv', # Remote Desktop Configuration
+		'TermService', # Remote Desjtop Service
+		'UmRdpService', # Remote Desktop Services UserMode Port Redirector
+		'RpcLocator', # Remote Procedure Call (RPC) Locator
+		'RetailDemo', # Retail Demo Service
+		'SCPolicySvc', # Smart Card Removal Policy
+		'SNMPTRAP', # SNMP Trap
+		'SharedRealitySvc', # Spatial Data Service
+		'WiaRpc', # Still Image Acquisition Events
+		'VacSvc', # Volumetric Audio Compositor Service
+		'WalletService', # WalletService
+		'wcncsvc', # Windows Connect Now
+		'Wecsvc', # Windows Event Collector
+		'perceptionsimulation', # Windows Perception Simulation Service
+		'WinRM', # Windows Remote Management (WS-Management)
+		'wmiApSrv', # WMI Performance Adapter
+		'WwanSvc', # WWAN AutoConfig
+		'XblAuthManager', # Xbox Live Auth Manager
+		'XboxNetApiSvc', # Xbox Live Networking Service
+		'RasAuto', # Remote Access Auto Connection Manager
+		'XblGameSave', # Xbox Live Game Save
+		'XboxGipSvc', # Xbox Accessory Management
+		'PushToInstall', # Windows PushToInstall Service
+		'spectrum', # Windows Perception Service
+		'icssvc', # Windows Mobile Hotspot Service
+		'wisvc', # Windows Insider Service
+		'WerSvc', # Windows Error Reporting Service
+		'FrameServer', # Windows Camera Frame Service
+		'WFDSConMgrSvc', # Wi-Fi Direct Services Connection Manager Service
+		'ScDeviceEnum', # Smart Card Device Enumeration Service
+		'SCardSvr', # Smart Card
+		'PhoneSvc', # Phone Service
+		'IpxlatCfgSvc', # IP Translation Configuration Service
+		'SharedAccess', # Internet Connection Sharing (ICS)
+		'vmicvss', # Hyper-V Volume Shadow Copy Requestor
+		'vmictimesync', # Hyper-V TIme Synchronization Service
+		'vmicrdv', # Hyper-V Remote Desktop Virtualization Service
+		'vmicvmsession', # Hyper-V PowerShell Direct Service
+		'vmicheartbeat', # Hyper-V Heartbeat Service
+		'vmicshutdown', # Hyper-V Guest Shudown Service
+		'vmicguestinterface', # Hyper-V Guest Service Interface
+		'vmickvpexchange', # Hyper-V Data Exchange Service
+		'HvHost', # HV Host Service
+		'FDResPub', # Function Discovery Resource Publication
+		'diagsvc', # Diagnostic Execution Service
+		'autotimesvc', # Cellular Time
+		'bthserv', # Bluetooth Support Service
+		'BTAGService', # Bluetooth Audio Gateway Service
+		'AssignedAccessManagerSvc', # AssignedAccessManager Service
+		'AJRouter', # AllJoyn Router Service
+		'lfsvc', # Geolocation Service
+		'CDPSvc', # Connected Devices Platform Service
+		'DiagTrack', # Connected User Experiences and Telemetry
+		'DPS', # Diagnostic Policy Service
+		'iphlpsvc', # IP Helper
+		'RasMan', # Remote Access Connection Manager
+		'SstpSvc', # Secure Socket Tunneling Protocol Service
+		'ShellHWDetection', # Shell Hardware Detection
+		'SSDPSRV', # SSDP Discovery
+		'WbioSrvc', # Windows Biometric Service
+		'stisvc' # Windows Image Acquisition (WIA)
+	Foreach ($Service in $Services) {
+		Set-Service -Name $Service -StartupType 'Disabled'
+		Stop-Service -Name $Service -Force
+	}
+	Write-Host 'Done.'
+}
+
+#################################################################
 Write-Host 'Tweaking your sytem based on your tweaks...'
 	Import-Module -Name '.\Tweaks.psm1'
 	$Tweaks | ForEach {
